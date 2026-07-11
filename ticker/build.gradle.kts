e153c0526f89dafb311c336dd4b388a28c736b0b
Joseph MJ <iamjosephmj@gmail.com>
2026-07-11 12:46:09 +0200

    Publish :ticker to GitHub Packages on release

--- ticker/build.gradle.kts
+++ ticker/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     alias(libs.plugins.android.library)
+    `maven-publish`
 }
+
+group = "tech.ssemaj"
+version = findProperty("VERSION_NAME")?.toString() ?: "1.0.0"
 
 android {
     namespace = "tech.ssemaj.ticker"
@@ -34,6 +38,45 @@ android {
         cmake {
             path = file("src/main/cpp/CMakeLists.txt")
             version = "3.22.1"
+        }
+    }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            artifactId = "ticker"
+            afterEvaluate { from(components["release"]) }
+            pom {
+                name = "ticker"
+                description = "Kernel-aligned wall-clock ticks for Android. Zero threads. One function."
+                url = "https://github.com/iamjosephmj/ticker"
+                developers {
+                    developer {
+                        id = "iamjosephmj"
+                        name = "Joseph MJ"
+                    }
+                }
+                scm {
+                    url = "https://github.com/iamjosephmj/ticker"
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/iamjosephmj/ticker")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR") ?: findProperty("gpr.user")?.toString()
+                password = System.getenv("GITHUB_TOKEN") ?: findProperty("gpr.key")?.toString()
+            }
         }
     }
 }
